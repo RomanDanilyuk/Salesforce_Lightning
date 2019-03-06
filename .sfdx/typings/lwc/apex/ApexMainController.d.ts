d687433ea63060620fa13fff2aad262f2980b525
Roman <rdanilyukr@gmail.com>
2019-03-06 16:31:27 +0300

    add .gitignore

--- .sfdx/typings/lwc/apex/ApexMainController.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "@salesforce/apex/ApexMainController.saveDataFile" {
-  export default function saveDataFile(param: {file: any}): Promise<any>;
-}
-declare module "@salesforce/apex/ApexMainController.getDataFile" {
-  export default function getDataFile(param: {ListId: any, pageNumber: any, pageLimit: any}): Promise<any>;
-}
-declare module "@salesforce/apex/ApexMainController.deleteSensors" {
-  export default function deleteSensors(param: {recordsToDelete: any}): Promise<any>;
-}
